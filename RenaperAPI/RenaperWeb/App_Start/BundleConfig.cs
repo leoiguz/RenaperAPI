e7e494b82222266f9b7896c9977bbf29ff6ace56
micael305 <miicael305@gmail.com>
2025-11-26 22:14:48 -0300

    chore(cleanup): eliminar archivos innecesarios de Content y actualizar BundleConfig

--- RenaperAPI/RenaperWeb/App_Start/BundleConfig.cs
+++ RenaperAPI/RenaperWeb/App_Start/BundleConfig.cs
@@ -1,30 +1,24 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace RenaperWeb
 {
     public class BundleConfig
     {
-        // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            // JQUERY (para validaciones standard de MVC)
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios.  De esta manera estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            // BUNDLE DE Renaper.css
+            bundles.Add(new StyleBundle("~/Content/renaper").Include(
+                      "~/Content/Renaper.css"));
         }
     }
 }
